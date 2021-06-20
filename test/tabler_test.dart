02d0982a1a8685c84f2363ab61b75216357720a3
yuzumone <yuzumone@outlook.com>
2021-06-20 15:29:24 +0900

    Update tests

--- test/tabler_test.dart
+++ test/tabler_test.dart
@@ -2,8 +2,8 @@ import 'package:tabler/tabler.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    test('First Test', () {
+  group('tabler test no header', () {
+    test('no header asii only', () {
       var t = Tabler();
       t.add(['a', 'b', 'c']);
       t.add([1, 22, 333]);
@@ -11,6 +11,193 @@ void main() {
 | a | b  | c   |
 | 1 | 22 | 333 |
 +---+----+-----+''';
+      expect(t.toString(), expected);
+    });
+
+    test('no header asii only setting padding', () {
+      var t = Tabler(style: TablerStyle(padding: 4));
+      t.add(['a', 'b', 'c']);
+      t.add([1, 22, 333]);
+      var expected = '''+---------+----------+-----------+
+|    a    |    b     |    c      |
+|    1    |    22    |    333    |
++---------+----------+-----------+''';
+      expect(t.toString(), expected);
+    });
+
+    test('no header asii only setting char', () {
+      var t = Tabler(
+        style: TablerStyle(
+          verticalChar: '!',
+          horizontalChar: '=',
+          junctionChar: '#',
+        ),
+      );
+      t.add(['a', 'b', 'c']);
+      t.add([1, 22, 333]);
+      var expected = '''#===#====#=====#
+! a ! b  ! c   !
+! 1 ! 22 ! 333 !
+#===#====#=====#''';
+      expect(t.toString(), expected);
+    });
+
+    test('no header asii only align right', () {
+      var t = Tabler(style: TablerStyle(align: TableTextAlign.right));
+      t.add(['a', 'b', 'c']);
+      t.add([1, 22, 333]);
+      var expected = '''+---+----+-----+
+| a |  b |   c |
+| 1 | 22 | 333 |
++---+----+-----+''';
+      expect(t.toString(), expected);
+    });
+
+    test('no header asii only align center', () {
+      var t = Tabler(style: TablerStyle(align: TableTextAlign.center));
+      t.add(['a', 'b', 'c']);
+      t.add([1, 22, 333]);
+      var expected = '''+---+----+-----+
+| a | b  |  c  |
+| 1 | 22 | 333 |
++---+----+-----+''';
+      expect(t.toString(), expected);
+    });
+
+    test('no header with multi bytes', () {
+      var t = Tabler();
+      t.add(['あ', '🎉🎉', 'aあ🎉']);
+      t.add([1, 22, 333]);
+      var expected = '''+----+------+-------+
+| あ | 🎉🎉 | aあ🎉 |
+| 1  | 22   | 333   |
++----+------+-------+''';
+      expect(t.toString(), expected);
+    });
+
+    test('no header with multi bytes align right', () {
+      var t = Tabler(style: TablerStyle(align: TableTextAlign.right));
+      t.add(['あ', '🎉🎉', 'aあ🎉']);
+      t.add([1, 22, 333]);
+      var expected = '''+----+------+-------+
+| あ | 🎉🎉 | aあ🎉 |
+|  1 |   22 |   333 |
++----+------+-------+''';
+      expect(t.toString(), expected);
+    });
+
+    test('no header with multi bytes align center', () {
+      var t = Tabler(style: TablerStyle(align: TableTextAlign.center));
+      t.add(['あ', '🎉🎉', 'aあ🎉']);
+      t.add([1, 22, 333]);
+      var expected = '''+----+------+-------+
+| あ | 🎉🎉 | aあ🎉 |
+| 1  |  22  |  333  |
++----+------+-------+''';
+      expect(t.toString(), expected);
+    });
+  });
+
+  group('tabler test with header', () {
+    test('with header asii only', () {
+      var t = Tabler();
+      t.addHeader(['a', 'b', 'c']);
+      t.add([1, 22, 333]);
+      var expected = '''+---+----+-----+
+| a | b  | c   |
++---+----+-----+
+| 1 | 22 | 333 |
++---+----+-----+''';
+      expect(t.toString(), expected);
+    });
+
+    test('with header asii only setting padding', () {
+      var t = Tabler(style: TablerStyle(padding: 4));
+      t.addHeader(['a', 'b', 'c']);
+      t.add([1, 22, 333]);
+      var expected = '''+---------+----------+-----------+
+|    a    |    b     |    c      |
++---------+----------+-----------+
+|    1    |    22    |    333    |
++---------+----------+-----------+''';
+      expect(t.toString(), expected);
+    });
+
+    test('with header asii only setting char', () {
+      var t = Tabler(
+        style: TablerStyle(
+          verticalChar: '!',
+          horizontalChar: '=',
+          junctionChar: '#',
+        ),
+      );
+      t.addHeader(['a', 'b', 'c']);
+      t.add([1, 22, 333]);
+      var expected = '''#===#====#=====#
+! a ! b  ! c   !
+#===#====#=====#
+! 1 ! 22 ! 333 !
+#===#====#=====#''';
+      expect(t.toString(), expected);
+    });
+
+    test('with header asii only align right', () {
+      var t = Tabler(style: TablerStyle(align: TableTextAlign.right));
+      t.addHeader(['a', 'b', 'c']);
+      t.add([1, 22, 333]);
+      var expected = '''+---+----+-----+
+| a |  b |   c |
++---+----+-----+
+| 1 | 22 | 333 |
++---+----+-----+''';
+      expect(t.toString(), expected);
+    });
+
+    test('with header asii only align center', () {
+      var t = Tabler(style: TablerStyle(align: TableTextAlign.center));
+      t.addHeader(['a', 'b', 'c']);
+      t.add([1, 22, 333]);
+      var expected = '''+---+----+-----+
+| a | b  |  c  |
++---+----+-----+
+| 1 | 22 | 333 |
++---+----+-----+''';
+      expect(t.toString(), expected);
+    });
+
+    test('with header with multi bytes', () {
+      var t = Tabler();
+      t.addHeader(['あ', '🎉🎉', 'aあ🎉']);
+      t.add([1, 22, 333]);
+      var expected = '''+----+------+-------+
+| あ | 🎉🎉 | aあ🎉 |
++----+------+-------+
+| 1  | 22   | 333   |
++----+------+-------+''';
+      expect(t.toString(), expected);
+    });
+
+    test('no header with multi bytes align right', () {
+      var t = Tabler(style: TablerStyle(align: TableTextAlign.right));
+      t.addHeader(['あ', '🎉🎉', 'aあ🎉']);
+      t.add([1, 22, 333]);
+      var expected = '''+----+------+-------+
+| あ | 🎉🎉 | aあ🎉 |
++----+------+-------+
+|  1 |   22 |   333 |
++----+------+-------+''';
+      expect(t.toString(), expected);
+    });
+
+    test('no header with multi bytes align center', () {
+      var t = Tabler(style: TablerStyle(align: TableTextAlign.center));
+      t.addHeader(['あ', '🎉🎉', 'aあ🎉']);
+      t.add([1, 22, 333]);
+      var expected = '''+----+------+-------+
+| あ | 🎉🎉 | aあ🎉 |
++----+------+-------+
+| 1  |  22  |  333  |
++----+------+-------+''';
       expect(t.toString(), expected);
     });
   });
